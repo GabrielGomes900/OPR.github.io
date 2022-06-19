702821292462cd72275507cc14531fb40c9da68d
☬ G̶ᴀ̶ʙ̶ʀ̶ɪ̶ᴇ̶ʟ̶G̶ᴏ̶ᴍ̶ᴇ̶s <gabrielgomesleite18@gmail.com>
2022-06-18 23:28:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+<html>
+  <head></head>
+<body>
+  <iframe src="https://evil.com","5F%62%6F%74%74%6F%6D"/>
 <script>
   window.open("https://evil.com","5F%62%6F%74%74%6F%6D");
   </script>
+</body>
+</html>
